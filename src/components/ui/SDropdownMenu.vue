e1fe87b4354ed58f98a750fa00057da5f31eb003
KyvYang <kyvyang@foxmail.com>
2026-08-09 02:34:01 +0800

    fix: 补充对子菜单项可见性的处理

--- src/components/ui/SDropdownMenu.vue
+++ src/components/ui/SDropdownMenu.vue
@@ -44,7 +44,14 @@ const emit = defineEmits<{
 }>();
 
 /** 显示的项 */
-const visibleItems = computed(() => props.items.filter((item) => item.show !== false));
+const visibleItems = computed(() => props.items.map(
+  (item) => {
+    if (item.children) {
+      return { ...item, children: item.children.filter((child) => child.show !== false) };
+    }
+    return item;
+  }
+));
 
 const handleSelect = (item: DropdownMenuItem): void => {
   if (item.disabled) return;
