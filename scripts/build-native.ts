b5903ea24ac28c2796dc0e4b498b4ccbe9d24ea2
imsyy <imsyy1024@gmail.com>
2026-08-14 15:57:29 +0800

    feat: 听歌识曲

--- scripts/build-native.ts
+++ scripts/build-native.ts
@@ -11,6 +11,10 @@ const modules: NativeModule[] = [
     name: "audio-engine",
   },
   {
+    name: "audio-capture",
+    enabled: process.platform === "win32",
+  },
+  {
     name: "media-ctrl",
   },
   {
